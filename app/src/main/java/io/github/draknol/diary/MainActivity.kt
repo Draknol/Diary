eac0213b9b27f79ce8a7e167ac708da04dbd0f9d
Draknol <reubenthomas.rr@gmail.com>
2025-10-05 20:36:42 +1300

    Added navigation functionality
    Added functionality to the navigation bar and FAB to swap between different pages.
    
    Generalised TitleBar to allow it to be reused for any page.
    
    Adjusted UI sizing and spacing.
    
    List state is now saved on rotation.

--- app/src/main/java/io/github/draknol/diary/MainActivity.kt
+++ app/src/main/java/io/github/draknol/diary/MainActivity.kt
@@ -4,25 +4,107 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import io.github.draknol.diary.ui.theme.DiaryTheme
 
+
+/**
+ * @author Reuben Russell - 23004666
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DiaryTheme {
+                // Navigation controller for the app
+                val navController = rememberNavController()
+                val currentBackStack by navController.currentBackStackEntryAsState()
+                val currentRoute = currentBackStack?.destination?.route
+
+                // Remembered state of the EntryList
+                val entryListState = rememberLazyGridState()
+
                 Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = { TitleBar() },
-                    bottomBar = { TabBar() },
-                    floatingActionButton = { AddButton(onClick = {}) }
+                    topBar = {
+                        when (currentRoute) {
+                            "home" -> TitleBar(
+                                title = "Diary",
+                                id = R.drawable.menu,
+                                contentDescription = "menu",
+                                onClick = { /* TODO */ }
+                            )
+                            "routine" -> TitleBar(
+                                title = "Reminders",
+                                id = R.drawable.menu,
+                                contentDescription = "menu",
+                                onClick = { /* TODO */ }
+                            )
+                            "add" -> TitleBar(
+                                title = "New Entry",
+                                id = R.drawable.back,
+                                contentDescription = "back",
+                                onClick = {
+                                    navController.navigate(route = "home") {
+                                        popUpTo(route = "home") { inclusive = false }
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+                        }
+                    },
+                    bottomBar = {
+                        when (currentRoute) {
+                            "home" -> TabBar(navController, currentPage = 0)
+                            "routine" -> TabBar(navController, currentPage = 1)
+                            else -> TablessBar()
+                        }
+                    },
+                    floatingActionButton = {
+                        if (currentRoute == "home") {
+                            AddButton(
+                                onClick = {
+                                    navController.navigate(route = "add") {
+                                        launchSingleTop = true
+                                    }
+                                }
+                            )
+                        }
+                    }
                 ) { innerPadding ->
-                    EntryList(modifier = Modifier.padding(paddingValues = innerPadding))
+                    NavHost(
+                        modifier = Modifier
+                            .padding(paddingValues = innerPadding)
+                            .fillMaxSize()
+                            .background(color = MaterialTheme.colorScheme.surfaceContainer),
+                        navController = navController,
+                        startDestination = "home",
+                        enterTransition = { EnterTransition.None },
+                        exitTransition = { ExitTransition.None },
+                    ) {
+                        composable(route = "home") {
+                            EntryList(state = entryListState)
+                        }
+                        composable(route = "routine") {
+                            /* TODO */
+                        }
+                        composable(route = "add") {
+                            /* TODO */
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/io/github/draknol/diary/MainPage.kt
+++ app/src/main/java/io/github/draknol/diary/MainPage.kt
@@ -15,29 +15,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import java.time.LocalDate
 
 
@@ -45,23 +44,29 @@ import java.time.LocalDate
  * Top app bar for the main page of the app.
  * Displays title `"Diary"`
  * Also has a non-functioning menu icon.
+ * @param title The title of the page.
+ * @param id The resource ID of the navigationIcon.
+ * @param contentDescription The description of the navigationIcon.
+ * @param onClick The action to be performed when the navigationIcon is clicked.
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TitleBar() {
+fun TitleBar(title: String, id: Int, contentDescription: String, onClick: () -> Unit) {
     TopAppBar(
         title = {
             Text(
-                text = "Diary",
-                fontSize = 36.sp
+                text = title,
+                fontSize = 32.sp
             )
         },
         navigationIcon = {
-            Icon(
-                painter = painterResource(id = R.drawable.menu),
-                contentDescription = "menu",
-                modifier = Modifier.padding(12.dp).size(36.dp)
-            )
+            IconButton(onClick) {
+                Icon(
+                    painter = painterResource(id),
+                    contentDescription = contentDescription,
+                    modifier = Modifier.padding(6.dp).size(36.dp)
+                )
+            }
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.surfaceContainer,
@@ -81,7 +86,7 @@ fun AddButton(onClick: () -> Unit) {
         onClick = onClick,
         shape = RoundedCornerShape(size = 24.dp),
         containerColor = MaterialTheme.colorScheme.primaryContainer,
-        modifier = Modifier.shadow(elevation = 16.dp)
+        elevation = FloatingActionButtonDefaults.elevation(defaultElevation = 4.dp)
     ) {
         Row (
             modifier = Modifier.width(width = 72.dp),
@@ -105,23 +110,12 @@ fun AddButton(onClick: () -> Unit) {
 /**
  * Bottom app bar for the main page of the app.
  * Displays two tabs: "Home" and "Routine".
+ * @param navController The navigation controller.
+ * @param currentPage The current page (0 for "home", 1 for "routine").
  */
 @Composable
-fun TabBar() {
-    // Will be replaced by navigation
-    var homeSelected by remember { mutableStateOf(value = true) }
-    var routineSelected by remember { mutableStateOf(value = false) }
-
-    // Get height of navigation bar
-    val navHeight = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
-
-    Box(
-        modifier = Modifier
-            .background(color = MaterialTheme.colorScheme.surfaceContainerHigh)
-            .fillMaxWidth()
-            .height(height = navHeight + 48.dp)
-    )
-
+fun TabBar(navController: NavController, currentPage: Int) {
+    TablessBar()
     BottomAppBar(
         modifier = Modifier
             .navigationBarsPadding()
@@ -130,25 +124,55 @@ fun TabBar() {
         contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
         content = {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier.fillMaxWidth().padding(bottom = 4.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 TabIconToggleButton(
                     id = R.drawable.home,
                     contentDescription = "home",
-                    checked = homeSelected,
-                    onClick = { homeSelected = true; routineSelected = false }
+                    checked = currentPage == 0,
+                    onClick = {
+                        if (currentPage != 0) {
+                            navController.navigate(route = "home") {
+                                popUpTo(route = "home") { inclusive = false }
+                                launchSingleTop = true
+                            }
+                        }
+                    }
                 )
-
                 TabIconToggleButton(
                     id = R.drawable.routine,
                     contentDescription = "routine",
-                    checked = routineSelected,
-                    onClick = { routineSelected = true; homeSelected = false }
+                    checked = currentPage == 1,
+                    onClick = {
+                        if (currentPage != 1) {
+                            navController.navigate(route = "routine") {
+                                popUpTo(route = "home") { inclusive = false }
+                                launchSingleTop = true
+                            }
+                        }
+                    }
                 )
             }
         }
+    )
+}
+
+
+/**
+ * Blank bottom app bar with no tabs.
+ */
+@Composable
+fun TablessBar() {
+    // Get height of navigation bar
+    val navHeight = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+
+    Box(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.surfaceContainerHigh)
+            .fillMaxWidth()
+            .height(height = navHeight + 48.dp)
     )
 }
 
@@ -162,18 +186,17 @@ fun TabBar() {
  */
 @Composable
 fun TabIconToggleButton(id: Int, contentDescription: String, checked: Boolean, onClick: () -> Unit) {
-    val color = if (checked) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.surfaceContainerHigh
-
     Surface(
         modifier = Modifier.size(width = 64.dp, height = 32.dp),
-        shape = RoundedCornerShape(24.dp),
-        color = color,
+        shape = RoundedCornerShape(size = 24.dp),
+        color = if (checked) MaterialTheme.colorScheme.secondaryContainer
+                else MaterialTheme.colorScheme.surfaceContainerHigh,
         onClick = onClick
     ) {
         Icon(
             painter = painterResource(id),
             contentDescription = contentDescription,
-            modifier = Modifier.padding(4.dp),
+            modifier = Modifier.padding(all = 4.dp),
             tint = MaterialTheme.colorScheme.onSurface
         )
     }
@@ -181,19 +204,19 @@ fun TabIconToggleButton(id: Int, contentDescription: String, checked: Boolean, o
 
 
 /**
- * Uses a `LazyVerticalGrid` to show a grid of `Entry`s.
- * @param modifier Modifier to be used in the `LazyVerticalGrid`.
+ * Lazy vertical grid of diary entries.
+ * @param state The state of the grid.
  */
 @Composable
-fun EntryList(modifier: Modifier = Modifier) {
+fun EntryList(state: LazyGridState) {
     LazyVerticalGrid(
-        modifier = modifier
-            .background(color = MaterialTheme.colorScheme.surfaceContainer),
-        columns = GridCells.Fixed(count = 1)
+        modifier = Modifier.background(color = MaterialTheme.colorScheme.surfaceContainer),
+        columns = GridCells.Fixed(count = 1),
+        state = state
     ) {
         val date = LocalDate.of(2025, 1, 1)
         items(count = 365) { index ->
-            Entry(date.plusDays(index.toLong() * 20), "Journal Entry $index")
+            Entry(date = date.plusDays(index.toLong() * 20), title = "Journal Entry $index")
         }
     }
 }
@@ -211,14 +234,14 @@ fun Entry(date: LocalDate, title: String) {
             .padding(horizontal = 8.dp, vertical = 4.dp)
             .background(
                 color = MaterialTheme.colorScheme.background,
-                shape = RoundedCornerShape(8.dp)
+                shape = RoundedCornerShape(size = 8.dp)
             )
             .padding(top = 16.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
     ) {
         Text(
             text = title,
             color = MaterialTheme.colorScheme.onBackground,
-            fontSize = 24.sp,
+            fontSize = 20.sp,
             overflow = TextOverflow.Ellipsis,
             maxLines = 1
         )
